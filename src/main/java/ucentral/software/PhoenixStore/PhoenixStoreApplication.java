9c353cc1d7fd213b0e07ab49f068390ee87e92a0
HandradeSinH <danielandrader23@gmail.com>
2025-04-05 08:16:53 -0500

    Master Commit

---
+++ src/main/java/ucentral/software/PhoenixStore/PhoenixStoreApplication.java
@@ -0,0 +1,13 @@
+package ucentral.software.PhoenixStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucentral/software/PhoenixStore/PhoenixStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ucentral.software.PhoenixStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
